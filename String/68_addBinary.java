c75198714c5542f5e0622ef7f8745a6a0c4abda4
Yoong <34851222+yj9676@users.noreply.github.com>
2019-12-29 23:22:22 +1030

    Update 68_addBinary.java

--- String/68_addBinary.java
+++ String/68_addBinary.java
@@ -1,1 +1,37 @@
+class Solution {
+    public String addBinary(String a, String b) {
 
+        StringBuilder sb=new StringBuilder();
+        int carry = 0;
+        
+        while(a.length() != b.length()){
+            if(a.length()<b.length()){
+                a = "0"+ a;
+            }else{
+                b = "0" + b;
+            } 
+        }
+               
+        char[] arraya = a.toCharArray();
+        char[] arrayb = b.toCharArray();
+        
+        for(int i=arraya.length-1;i>=0;i--){
+            int x = arraya[i] - '0';
+            int y = arrayb[i] - '0';
+            int temp = x + y + carry;
+            if(temp>=2){
+               carry = 1;
+               sb.append(temp-2);
+            }else{
+               sb.append(temp);
+               carry = 0;
+            }
+        }
+        
+        if(carry == 1){
+            sb.append("1");
+        }
+        
+        return sb.reverse().toString();
+}
+}
